f125d7286acf6b14fd20a2fec50057fa64525878
vormond <163526130+vormond@users.noreply.github.com>
2024-03-16 08:40:29 +0000

    Поисковая строка- новый фоновый цвет, новый цвет обводки, закругления

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -7,16 +7,24 @@ function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
     const Button = styled.div`
-        background: #4B759E;
-        border: 1px solid #152842;
+        background: #ffffff;
+        border: 1px solid #64748B;
+        border-radius: 24px; /* Скругление */
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
-        min-width: ${minWidth};
-        color: white;
+        min-width: 34px;
+        color: #64748B; /* Цвет текста */
+        font-family: 'Inter Medium', sans-serif; /* Шрифт */
+        padding: 12px 24px; /* Отступы */
+        cursor: pointer; /* Указатель при наведении */
         &:hover {
             background: linear-gradient(180deg, #4D9FF0 0%, #36699D 100%);
+            border-color: #64748B;
+            color: #ffffff;
         }
         &:active {
             background: #152842;
+            border-color: #152842;
+            color: #ffffff;
         }
     `;
     return (
--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -9,11 +9,11 @@ const WrapperInput = styled.div`
 
 const Input = styled.input`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #152842;
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    background-color: #F8FAFC;
     width: 100%;
+    padding: 8px; /* Adjusted padding */
     &:hover {
         background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
     }
@@ -23,20 +23,18 @@ const Input = styled.input`
 `;
 
 const SearchIconWrapper = styled.div`
-    background-color: #4B759E;
     position: absolute;
-    box-sizing:border-box;
-    right: 0px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
-    top:50%;
+    right: 0px;
+    top: 50%;
     transform: translateY(-50%);
 `;
 
 function SearchField() {
     return (
-        <WrapperInput className='d-inline-flex'>  {/* Устанавливает ширину поля и включает относительное положение */}
-            <Input className='p-2' placeholder='Найти на странице...'/>
-            <SearchIconWrapper className='p-2'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
-                <IconOnlyButton icon={<SearchIcon/>}/>
+        <WrapperInput className='d-inline-flex'>  
+            <Input className='p-2' placeholder='Найти'/>
+            <SearchIconWrapper className='p-2'>
+                <IconOnlyButton icon={<SearchIcon />} />
             </SearchIconWrapper>
         </WrapperInput>
     );
