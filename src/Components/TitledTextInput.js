7586392996ada5a3206e105d54056a6945724693
vormond <163526130+vormond@users.noreply.github.com>
2024-03-17 21:27:18 +0000

    Обновил заполняемые поля с их заголовками

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -2,19 +2,20 @@ import styled from "styled-components";
 
 const Input = styled.input`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    background-color: #F8FAFC;
     color: #666787;
     &:hover {
-        background-color: #D9E2EFB8;
+        border: solid #E2E8F0 1px;
+        background-color: #F8FAFC;
     }
 `;
 
 function TitledTextInput({title, def}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<div className= 'caption' >{title}</div>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
